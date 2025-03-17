b7fbcd2c552dc637de4719163b1eebce982afe76
nabiilahaulasafda <nabiilahaulasafdaa@gmail.com>
2025-03-17 12:45:19 +0700

    menambahkan halaman login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="col-2 m-auto">
+        <div class="row mt-5">
+    <div class="card text-center mb-3" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title"> LOGIN </h5>
+          <div class="form-floating mb-4 row mt-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating mb-4 row mt-3">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+        </div>
+
+          <a href="#" class="btn btn-primary"> login </a>
+        </div>
+      </div>
+    </div>
+</div>
+</body>
+</html>
+
+                    {{-- <div class="form-floating mb-3 row mt-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email address</label>
+                      </div>
+                      <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                    </div> --}}
+
+                    {{-- <div class="col-4 m-auto">
+                        <div class="row mt-5">
+                            <div class="card text-center rounded">
+                                <div class="card-header">
+                                    LOGIN
+                                </div>
+                                    <div class="card-body rounded">
+                                        <div class="form-floating mb-3 row mt-3">
+                                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                            <label for="floatingInput">Email address</label>
+                                        </div>
+                                        <div class="form-floating mb-3 row mt-3">
+                                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                            <label for="floatingPassword">Password</label>
+                                        </div>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                            </div>
+                        </div>
+                    </div> --}}
+
+
+
+
+
+
+
