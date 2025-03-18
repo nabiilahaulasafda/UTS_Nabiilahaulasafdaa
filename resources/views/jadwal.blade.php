06f9792a3b6cbd4aab126abe8089bd98e5fe35b0
nabiilahaulasafda <nabiilahaulasafdaa@gmail.com>
2025-03-18 15:07:09 +0700

    menambahkan menu jadwal
    jadwal yang dibuat oleh petugas ketika mahasiswa sudah disetujui magang

--- resources/views/jadwal.blade.php
+++ resources/views/jadwal.blade.php
@@ -1,32 +1,71 @@
 @extends('layout.master')
 @section('title')
-    Beranda
+    Jadwal
 @endsection
 
-@section('beranda', 'active')
+@section('jadwal', 'active')
 
 @section('isi')
-<ol class="list-group list-group-numbered">
-    <li class="list-group-item d-flex justify-content-between align-items-start">
-      <div class="ms-2 me-auto">
-        <div class="fw-bold">Subheading</div>
-        Content for list item
-      </div>
-      <span class="badge text-bg-primary rounded-pill">14</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-start">
-      <div class="ms-2 me-auto">
-        <div class="fw-bold">Subheading</div>
-        Content for list item
-      </div>
-      <span class="badge text-bg-primary rounded-pill">14</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-start">
-      <div class="ms-2 me-auto">
-        <div class="fw-bold">Subheading</div>
-        Content for list item
-      </div>
-      <span class="badge text-bg-primary rounded-pill">14</span>
-    </li>
-  </ol>
+
+
+    <div class="container rounded mt-5" style="width: 500px">
+        <div class="card">
+            {{-- <div class="card-header">
+                <h2> </h2>
+            </div> --}}
+                <div class="card-body bg-dark rounded">
+                    <div class="row text-white text-left">
+                        <div class="col mt-3 mx-3">
+                            <form class="p-3">
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">Kode Jadwal</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">NIP Petugas</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">Nim Mahasiswa</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">Mulai Magang</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">Selesai Magang</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col mt-3 mx-3">
+                            <form class="p-3">
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">Senin</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">Selasa</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">Rabu</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">Kamis</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="disabledTextInput" class="form-label">Jumat</label>
+                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+
 @endsection
