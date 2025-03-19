22752d74c4ebdcf5558e9e739db19f664f9c58f3
nabiilahaulasafda <nabiilahaulasafdaa@gmail.com>
2025-03-19 11:06:04 +0700

    Update sertifikat.blade.php

--- resources/views/sertifikat/sertifikat.blade.php
+++ resources/views/sertifikat/sertifikat.blade.php
@@ -6,19 +6,45 @@
 @section('sertifikat', 'active')
 
 @section('isi')
-<ul class="list-group">
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      A list item
-      <span class="badge text-bg-primary rounded-pill">14</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      A second list item
-      <span class="badge text-bg-primary rounded-pill">2</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      A third list item
-      <span class="badge text-bg-primary rounded-pill">1</span>
-    </li>
-  </ul>
+
+<div class="container center col-6 col-sm-4">
+    <div class="card mt-5">
+        <div class="card-header float-start">
+              <h2> DAFTAR SERTIFIKAT </h2>
+        </div>
+        <div class="card-body">
+            <div class="row g-3 ">
+                <div class="input-group mb-3 g-3 ">
+                    <input class="form-control" type="text" placeholder="User 1" aria-label="Disabled input example" disabled aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><a class="nav-link"  href="/open"> Open </a> </button>
+                </div>
+                <div class="input-group mb-3 g-3 ">
+                    <input class="form-control" type="text" placeholder="User 2" aria-label="Disabled input example" disabled aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"> <a class="nav-link" href="/open"> Open </a> </button>
+                </div>
+                <div class="input-group mb-3 g-3 ">
+                    <input class="form-control" type="text" placeholder="User 3" aria-label="Disabled input example" disabled aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><a class="nav-link" href="/open"> Open </a> </button>
+                </div>
+                <div class="input-group mb-3 g-3 ">
+                    <input class="form-control" type="text" placeholder="User 4" aria-label="Disabled input example" disabled aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><a class="nav-link" href="/open"> Open </a> </button>
+                </div>
+                <div class="input-group mb-3 g-3 ">
+                    <input class="form-control" type="text" placeholder="User 5" aria-label="Disabled input example" disabled aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><a class="nav-link" href="/open"> Open </a> </button>
+                </div>
+                <div class="input-group mb-3 g-3 ">
+                    <input class="form-control" type="text" placeholder="User 6" aria-label="Disabled input example" disabled aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><a class="nav-link" href="/open"> Open </a></button>
+                </div>
+                <div class="input-group mb-3 g-3 ">
+                    <input class="form-control" type="text" placeholder="User 7" aria-label="Disabled input example" disabled aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><a class="nav-link" href="/open"> Open </a></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
